c89a202e9b73533ce9f66d93c68353e18e483af1
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2023-04-17 21:32:12 +0700

    a

--- Day26_MVC_Demo_Website/Codes_demo/mvc_demo/controllers/Controller.php
+++ Day26_MVC_Demo_Website/Codes_demo/mvc_demo/controllers/Controller.php
@@ -1,1 +1,23 @@
-<!--Controller.php-->
+<!--controllers/Controller.php
+-->
+<?php
+// Class cha để cho các class kế thừa từ nó
+class Controller {
+    // Tiêu đề trang
+    public $page_title;
+    // Lỗi
+    public $error;
+    // Lưu nội dung file view tương ứng với chức năng:
+    public $content;
+
+    // Lấy nội dung file kèm cơ chế truyền biến tường minh
+    // $file_path: đường dẫn file view
+    // $variables: mảng chứa các biến cần truyền vào view
+    public function render($file_path, $variables = []) {
+        extract($variables);
+        ob_start();
+        require $file_path;
+        return ob_get_clean();
+        //file_get_contents
+    }
+}
